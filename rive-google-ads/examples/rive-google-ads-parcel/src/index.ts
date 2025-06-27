b97d7556a01a0eb1063d8ba1e0bf16f870e6f85a
Lance <lance@lancesnider.com>
2025-06-27 16:17:04 -0700

    Testing

--- rive-google-ads/examples/rive-google-ads-parcel/src/index.ts
+++ rive-google-ads/examples/rive-google-ads-parcel/src/index.ts
@@ -1,61 +1,59 @@
-import { Rive, Fit, Layout, Alignment } from "@rive-app/canvas-single";
+import { Rive, Fit, Layout } from '@rive-app/canvas-single'
+import { deviceOrientationSupported } from './deviceOrientationSupported'
 
-const WorldCreatorFile = new URL("/assets/world_creator.riv", import.meta.url);
-const DisneyFile = new URL("/assets/disney.riv", import.meta.url);
+// const BikeFile = new URL('/assets/sport_bike_ad.riv', import.meta.url)
+const WorldCreatorFile = new URL('/assets/world_creator.riv', import.meta.url)
 
 // ---------------------------------
-// The layout the graphic will adhere to
-const layout = new Layout({
-  fit: Fit.Layout, // Change to: rive.Fit.Contain, or Cover
-  // alignment: Alignment.Center,
-  // layoutScaleFactor: 2,
-});
+// HTML Canvas element to render to
+const riveCanvas = document.getElementById('riveCanvas') as HTMLCanvasElement
 
 // ---------------------------------
 // Resize on widow
 function resizeRiveCanvas(riveInstance: Rive) {
   window.addEventListener(
-    "resize",
+    'resize',
     () => {
-      riveInstance.resizeDrawingSurfaceToCanvas();
+      riveInstance.resizeDrawingSurfaceToCanvas()
     },
     false
-  );
+  )
 }
 
 // ---------------------------------
 // Randomly select a Rive file
 function getRandomRiveFile(): URL {
-  const files = [WorldCreatorFile, DisneyFile];
-  const randomIndex = Math.floor(Math.random() * files.length);
-  return files[randomIndex];
+  const files = [WorldCreatorFile]
+  const randomIndex = Math.floor(Math.random() * files.length)
+  return files[randomIndex]
 }
 
-// ---------------------------------
-// HTML Canvas element to render to
-const riveCanvas = document.getElementById("riveCanvas") as HTMLCanvasElement;
-
 async function loadRiveFile() {
-  const RiveFileToLoad = getRandomRiveFile();
-  return await (await fetch(new Request(RiveFileToLoad))).arrayBuffer();
+  const RiveFileToLoad = getRandomRiveFile()
+  return await (await fetch(new Request(RiveFileToLoad))).arrayBuffer()
 }
 
 async function setup() {
-  const bytes = await loadRiveFile();
+  const bytes = await loadRiveFile()
+
+  const useGyroscope = await deviceOrientationSupported()
 
   const riveInstance = new Rive({
     buffer: bytes,
     autoplay: true,
     autoBind: true,
     canvas: riveCanvas,
-    layout: layout,
-    stateMachines: ["State Machine 1"],
+    layout: new Layout({
+      fit: Fit.Layout,
+      layoutScaleFactor: 1,
+    }),
+    stateMachines: ['State Machine 1'],
     onLoad: () => {
-      resizeRiveCanvas(riveInstance);
+      resizeRiveCanvas(riveInstance)
       // Prevent a blurry canvas by using the device pixel ratio
-      riveInstance.resizeDrawingSurfaceToCanvas();
+      riveInstance.resizeDrawingSurfaceToCanvas()
     },
-  });
+  })
 }
 
-setup();
+setup()
